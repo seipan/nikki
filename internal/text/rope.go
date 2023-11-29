f2a1d0f81e2a5a38fa4fb41f6b71b4f3d45c293c
seipan <yamasakipann0218@gmail.com>
2023-11-30 02:08:38 +0900

    fix(text) : lint pass

--- internal/text/rope.go
+++ internal/text/rope.go
@@ -3,18 +3,18 @@ package text
 import "github.com/vinzmay/go-rope"
 
 // To handle a large amount of text data, we use a data structure called 'rope' for storing it in a database.
-// Rope is an effective algorithm for long text data, commonly adopted in text editors
-type TextRope struct {
+// Rope is an effective algorithm for long text data, commonly adopted in text editors.
+type textRope struct {
 	rope *rope.Rope
 }
 
-// String returns the text as a string
-func (t *TextRope) String() string {
+// String returns the text as a string.
+func (t *textRope) String() string {
 	return t.rope.String()
 }
 
 func NewText(text string) Text {
-	return &TextRope{
+	return &textRope{
 		rope.New(text),
 	}
 }

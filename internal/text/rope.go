61e86245497368e4affb8a088d0c93df165618d3
seipan <yamasakipann0218@gmail.com>
2023-11-30 01:58:32 +0900

    fix(text) : interface Text Add

--- internal/text/rope.go
+++ internal/text/rope.go
@@ -4,17 +4,17 @@ import "github.com/vinzmay/go-rope"
 
 // To handle a large amount of text data, we use a data structure called 'rope' for storing it in a database.
 // Rope is an effective algorithm for long text data, commonly adopted in text editors
-type Text struct {
+type TextRope struct {
 	rope *rope.Rope
 }
 
 // String returns the text as a string
-func (t *Text) String() string {
+func (t *TextRope) String() string {
 	return t.rope.String()
 }
 
-func NewText(text string) *Text {
-	return &Text{
+func NewText(text string) Text {
+	return &TextRope{
 		rope.New(text),
 	}
 }
